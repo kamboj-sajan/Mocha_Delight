646abf724e62d8b19c938a73f48c22eb8a43c96f
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2024-06-09 04:21:05 +0530

    5

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-import ReactDom from "react-dom";
-import App from "./components/app";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
 
-ReactDom.render(<div>
-  <App />
-</div>,document.getElementById("root"));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
