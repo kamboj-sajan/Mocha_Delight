1c9e4c408bc6f542def0d481c67d95dce16f8eae
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2024-06-10 00:00:22 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,22 +5,27 @@ import Body3 from "./Body3";
 import Body4 from "./Body4";
 import "../css/Home.css"
 
+const shop_name = "Mocha delight";
+const shop_address = "Hotel Aquamarine, Ground Floor, Himalaya Marg, Sector 22C, Chandigarh, 160022";
+const shop_phone = "+03504555";
+const shop_timings = "9:00-20:00";
 
-function Home({ shop }) {
+
+function Home() {
   return (
-    <div>
+     <div>
       <Body1 />
       <Body2 /> 
       <div id="mapc">
       <div className="address">
-        <h1>{shop.name}</h1>
-        <p>{shop.address}</p>
-        <p>Phone: {shop.phone}</p>
-        <p>Open Hours: {shop.open_hours}</p>
+        <h1>{shop_name}</h1>
+        <p>{shop_address}</p>
+        <p>Phone: {shop_phone}</p>
+        <p>Open Hours: {shop_timings}</p>
         <div className="map">
           <iframe
             title="map"
-            src={`https://www.google.com/maps?q=${encodeURIComponent(shop.address)}&output=embed`}
+            src={`https://www.google.com/maps?q=${encodeURIComponent(shop_address)}&output=embed`}
             width="600"
             height="450"
             style={{ border: 0 }}
